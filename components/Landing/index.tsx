39458099696bda43b6e30e8d9d231b7db4b35205
Douglass Hooker <DouglassHooker@gmail.com>
2019-03-26 21:04:53 -0400

    feat: add landing page

---
+++ components/Landing/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Landing.scss';
+
+const Landing: React.SFC = () => {
+  return <div className="landing-wrapper">Landing</div>;
+};
+
+export default Landing;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Landing from '../components/Landing';
+
 const Index: React.SFC = () => {
   return (
     <div>
+      <Landing />
       <p>Hello from Next!</p>
     </div>
   );
